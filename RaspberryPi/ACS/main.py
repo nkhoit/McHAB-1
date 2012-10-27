3a3bb27d6353d0e52cd8d92cc8f1fc6aac933c3d
HUmar104 <HUmar104@gmail.com>
2012-10-27 12:48:31 -0400

    Tried to break into multiple functions

--- RaspberryPi/ACS/main.py
+++ RaspberryPi/ACS/main.py
@@ -17,13 +17,6 @@ F_sample = 50
 T_sample = 1.0/F_sample
 GYRO_FS = 1000 #16 bit register
 
-Cbi_hat = np.identity(3)
-g_i = np.array([0,0,-1])
-b_d = np.array([0.91651524 , 0.38360686 , 0.11333839])
-C_d = np.identity(3)
-
-counter=0
-initial=time.time()*1000.0
 
 def change_frame(matrix):
     frame=np.array([-1,0,0],[0,-1,0],[0,0,-1])
@@ -44,7 +37,58 @@ def convert_gyro(array):
 def cross(array):
     return np.array([[0, -array[2], array[1]],[array[2],0,-array[0]],[-array[1],array[0],0]])
 
+def parseData(line_u):
+
+    raw_accel=np.array([-line_u[0],-line_u[1],-line_u[2]])
+    norm_accel=normalize(raw_accel)
+
+    raw_gyro=np.array([line_u[3],line_u[4],line_u[5]])
+    omega_measured=convert_gyro(raw_gyro)
+
+    raw_magne=np.array([line_u[6],line_u[7],line_u[8]])
+    norm_magne=normalize(raw_magne)
+    norm_magne=np.array([-norm_magne[0],-norm_magne[1],norm_magne[2]])
+
+    return norm_accel,omega_measured,norm_magne
+
+def updateAttitude(Cbi_hat,b_d,g_i,norm_accel,omega_measured,norm_magne):
+    b_b=np.dot(Cbi_hat,b_d.T)
+    g_b=np.dot(Cbi_hat,g_i.T)
+
+    r=-10*(np.dot(cross(g_b),norm_accel.T) + np.dot(cross(b_b),norm_magne.T))
+
+    omega_hat = omega_measured+r
+    omega_hat_mag = math.sqrt(omega_hat[0]**2+omega_hat[1]**2+omega_hat[2]**2)
+    omega_hat_mag_r = 1.0/omega_hat_mag
+
+    Ak = np.eye(3) - cross(omega_hat)*math.sin(omega_hat_mag*T_sample)*omega_hat_mag_r + (1-math.cos(omega_hat_mag*T_sample))*np.dot(cross(omega_hat),cross(omega_hat))*omega_hat_mag_r**2
+
+    Cbi_hat_new = np.dot(Ak,Cbi_hat)
+
+    euler=tf.euler_from_matrix(Cbi_hat_new, axes='szyx')
+    euler=tuple([x*180/math.pi for x in euler])
+
+    return Cbi_hat_new,b_b,g_b
+
+def updateControl(Cbi_hat,C_d,b_d,g_i,b_b,g_b,omega_measured):
+    b_body_d = np.dot(C_d,b_d.T)
+    g_body_d = np.dot(C_d,g_i.T)
+
+    u_p = 0.05*(np.dot(cross(g_body_d),g_b.T) + np.dot(cross(b_body_d),b_b.T))
+    u_d = -0.05*omega_measured[2]
+
+    u_p = np.dot(np.dot(u_p,Cbi_hat),np.array([0,0,1]).T)
+
+    tau = u_d + u_p
+
+    return tau
+
 if __name__ == '__main__':
+    Cbi_hat = np.identity(3)
+    g_i = np.array([0,0,-1])
+    b_d = np.array([0.91651524 , 0.38360686 , 0.11333839])
+    C_d = np.identity(3)
+
     try:
         ser=serial.Serial(COM,BAUD,timeout=1)
     except:
@@ -56,65 +100,13 @@ if __name__ == '__main__':
         if(len(line)==18):
             line_u=struct.unpack('hhhhhhhhh', line)
 
-            raw_accel=np.array([-line_u[0],-line_u[1],-line_u[2]])
-            norm_accel=normalize(raw_accel)
+            norm_accel,omega_measured,norm_magne=parseData(line_u)
 
-            raw_gyro=np.array([line_u[3],line_u[4],line_u[5]])
-            omega_measured=convert_gyro(raw_gyro)
+            Cbi_hat_new,b_b,g_b=updateAttitude(Cbi_hat,g_i,b_d,norm_accel,omega_measured,norm_magna)
 
-            raw_magne=np.array([line_u[6],line_u[7],line_u[8]])
-            norm_magne=normalize(raw_magne)
-            norm_magne=np.array([-norm_magne[0],-norm_magne[1],norm_magne[2]])
+            tau=updateControl(Cbi_hat,C_d,b_d,g_i,b_b,g_b,omega_measured)
 
-            b_b=np.dot(Cbi_hat,b_d.T)
-            g_b=np.dot(Cbi_hat,g_i.T)
-
-            r=-10*(np.dot(cross(g_b),norm_accel.T) + np.dot(cross(b_b),norm_magne.T))
-
-            omega_hat = omega_measured+r
-            omega_hat_mag = math.sqrt(omega_hat[0]**2+omega_hat[1]**2+omega_hat[2]**2)
-            omega_hat_mag_r = 1.0/omega_hat_mag
-
-            Ak = np.eye(3) - cross(omega_hat)*math.sin(omega_hat_mag*T_sample)*omega_hat_mag_r + (1-math.cos(omega_hat_mag*T_sample))*np.dot(cross(omega_hat),cross(omega_hat))*omega_hat_mag_r**2
-
-            Cbi_hat_new = np.dot(Ak,Cbi_hat)
-
-            euler=tf.euler_from_matrix(Cbi_hat_new, axes='szyx')
-            euler=tuple([x*180/math.pi for x in euler])
-
-            #print str(euler)
-            print str(Cbi_hat)
-
-            b_body_d = np.dot(C_d,b_d.T)
-            g_body_d = np.dot(C_d,g_i.T)
-            #print str(b_body_d)+'\n'+str(g_body_d)
-
-            u_p = 0.05*(np.dot(cross(g_body_d),g_b.T) + np.dot(cross(b_body_d),b_b.T))
-            u_d = -0.05*omega_measured[2]
-
-            u_p = np.dot(np.dot(u_p,Cbi_hat),np.array([0,0,1]).T)
-
-            tau = u_d + u_p
-            #print str(tau)
-
-            Cbi_hat = Cbi_hat_new
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Cbi_hat=Cbi_hat_new
 
 
 
