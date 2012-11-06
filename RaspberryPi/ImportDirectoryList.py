6ab7f31397de2a0ba9c6416d78d37b7b036c7ced
00long00 <longma4000@hotmail.com>
2012-11-05 22:00:03 -0500

    Update directory list file

--- RaspberryPi/ImportDirectoryList.py
+++ RaspberryPi/ImportDirectoryList.py
@@ -2,6 +2,7 @@ import sys
 
 sys.path.append("./ACS")
 sys.path.append("./Buzzer")
+sys.path.append("./CutDown")
 sys.path.append("./MiniIMU")
 sys.path.append("./SocketTest")
 sys.path.append("./TestingScript")
