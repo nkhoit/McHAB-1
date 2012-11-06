09339430c11924fe257b4e7a40fc7577df44ab59
00long00 <longma4000@hotmail.com>
2012-11-05 21:57:15 -0500

    Cut Down Implementation 1

---
+++ RaspberryPi/CutDown/CutDown.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+
+class CutDown:
+	delay = 60000 #1 minutes
+
+	def __init__(self, pin_number, cut_down_time):
+		self.pin = pin_number #Set cut down GPIO pin
+		self.cut_down_time = cut_down_time #Set cut down time
+		self.previous_time = 0 #previous time. Used to time the amount of time to set the voltage high
+		self.status = 0 #current status of the cut down. 0 = idle 1 = wire is cut
+
+		#Setup GPIO
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(pin_number, GPIO.OUT)
+		GPIO.output(pin_number, GPIO.LOW)
+
+	def cut(self, current_time):
+		#if cut down time is reached, set the output to high for a short interval of time. If the line is cut, do not attempt to perform anymore cut.
+		if(self.status != 1 && current_time > self.cut_down_time):
+			#Start timer and turn on resistor 
+			if(self.previous_time == 0):
+				self.previous_time = current_time
+				GPIO.output(self.pin, GPIO.HIGH) #BURN!!!!!!!
+				self.previous_time = current_time; #Start timer
+				
+			#Resistor is on for an amount determined by delay and then will be shut off
+			else if(current_time - self.previous_time > delay):
+				GPIO.output(self.pin, GPIO.LOW) #STOP BURNING
+				self.status = 1; #Wire is cut
+				
+		
--- RaspberryPi/McHab2012.py
+++ RaspberryPi/McHab2012.py
@@ -5,16 +5,19 @@ import RPi.GPIO as GPIO
 #import user libraries
 from ImportDirectoryList import * #import all used directories
 import Buzzer 
+import CutDown
 
 class McHab2012:
 	#Constants: To do, change format
 	data_read_time = 20 #50 Hz
 	buzzer_time = 1000 #1 minutes
 	cut_time = 200 #5 Hz
+	buzzer_pin = 25 #GPIO pin 25
+	cut_down_pin 
 
 	def __init__(self):
-		#Create Buzzer Object
-		self.buzzer = Buzzer.Buzzer(25)
+		self.buzzer = Buzzer.Buzzer(self.buzzer_pin) #Create Buzzer Object
+		self.CutDown = CutDown.CutDown(self.cut_down_pin) #Create CutDown Object
 		
 		#Timer Variables
 		self.current_time = time.time()*1000.0
@@ -32,7 +35,7 @@ class McHab2012:
 			self.buzzer.loop(self.current_time, 5) #Run buzzer subroutine
 			self.previous_buzzer_time = self.current_time #reset timer
 		if(self.current_time - self.previous_cut_time > self.cut_time):
-			#Run cut down subroutine
+			self.cut(self.current_time) #Run cut down subroutine
 			self.previous_cut_time = self.current_time #reset timer
 
 if __name__ == '__main__':
