c334620e2244e4f32ba99b9d532e3584b218f55c
00long00 <longma4000@hotmail.com>
2012-11-06 19:10:22 -0500

    Implemented Timer

--- RaspberryPi/Buzzer/Buzzer.py
+++ RaspberryPi/Buzzer/Buzzer.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+sys.path.append("../")
+import Timer
 
 class Buzzer:
-	delay = 1000 #1 second delay between beep
-
-	def __init__(self, pin_number, current_time, start_time, altitude_threshold):
+	def __init__(self, pin_number, start_time, beeping_delay, altitude_threshold):
 		self.pin = pin_number
 		self.start_time = start_time
 		self.altitude_threshold = altitude_threshold
+		self.timer_start = Timer.Timer(start_time)
+		self.timer_delay = Timer.Timer(beeping_delay)
 
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(pin_number, GPIO.OUT)
 		GPIO.output(pin_number, GPIO.LOW)
 		
 		self.toggle = 0 #Default toggle for delayed beeping. 0 = off, 1 = on
-		self.previous_time = current_time #Set previous time for delayed beeping
 		
-	def loop(self, current_time, altitude):
-		if(not (current_time < self.start_time) and altitude < self.altitude_threshold):
-			self.beep_delay(current_time)
-		else:
-			GPIO.output(self.pin, GPIO.LOW)
-			
-			
-	def beep_delay(self, current_time):
+		#Start timer_start
+		self.timer_start.start_timer()
+		
+	def loop(self, altitude):
+		if(self.timer_start.get_flag == 0 && altitude < self.altitude_threshold):
+			self.beep_delay()
+					
+	def beep_delay(self):
 		#Delay the beep for the self.delay amount
-		if(current_time - self.previous_time > self.delay):	
-			self.previous_time = current_time #reset timer
-			
+		if(self.timer_delay.previous_time == -1):
+			self.timer_delay.start_timer()
+		
+		elif(self.timer_delay.get_flag == 0):
 			if(self.toggle == 0):
 				self.toggle = 1 #set toggle to 1 for next iteration
 				GPIO.output(self.pin, GPIO.LOW)
@@ -35,4 +37,4 @@ class Buzzer:
 			else:
 				self.toggle = 0 #set toggle to 0 for next iteration
 				GPIO.output(self.pin, GPIO.HIGH)
-				print "HIGH"
+				print "HIGH"			
--- RaspberryPi/McHab2012.py
+++ RaspberryPi/McHab2012.py
@@ -1,60 +1,75 @@
 #!/usr/bin/python
-import time
+import Timer
 import RPi.GPIO as GPIO
 
 #import user libraries
 from ImportDirectoryList import * #import all used directories
 import Buzzer 
-import CutDown
+#import CutDown
 
 class McHab2012:
 	#Constants: To do, change format
-	system_start_time = 3000 #3 seconds
-	data_read_time = 20 #50 Hz
-	buzzer_time = 1000 #1 minutes
-	cut_timer = 1000
-	cut_time = 10000 #0.5	 minutes 
-	buzzer_pin = 23 #GPIO pin 23
-	cut_down_pin = 18 #GPIO pin 18
-	buzzer_start_time = 1800000 #30 minutes
-	altitude_threshold = 500 #500 meters
+	# system_start_time = 3000 #3 seconds
+	# data_read_time = 20 #50 Hz
+	# buzzer_time = 1000 #1 minutes
+	# cut_timer = 1000
+	# cut_time = 10000 #0.5	 minutes 
+	# buzzer_pin = 23 #GPIO pin 23
+	# cut_down_pin = 18 #GPIO pin 18
+	# buzzer_start_time = 1800000 #30 minutes
+	# altitude_threshold = 500 #500 meters
+	
+	cut_down_time = 10000
+	buzzer_start = 30000
 
 	def __init__(self):		
+		#self.timer_system_start = Timer.Timer(3000)
+		self.timer_buzzer_poll = Timer.Timer(1000)
+		#self.timer_cut_poll = Timer.Timer(1000)
+	
+		self.buzzer = Buzzer.Buzzer(self.buzzer_pin, self.buzzer_start, self.altitude_threshold) #Create Buzzer Object
+		#self.CutDown = CutDown.CutDown(self.cut_down_pin, self.cut_down_time) #Create CutDown Object	
+	
 		#Timer Variables
-		self.current_time = time.time()*1000.0
-		self.previous_data_read_time = self.current_time
-		self.previous_buzzer_time = self.current_time
-		self.previous_cut_time = self.current_time
+		# self.current_time = time.time()*1000.0
+		# self.previous_data_read_time = self.current_time
+		# self.previous_buzzer_time = self.current_time
+		# self.previous_cut_time = self.current_time
 		
 		#timer offset
-		self.cut_time += self.current_time
-		self.buzzer_start_time += self.current_time
-		self.system_start_time += self.current_time
+		# self.cut_time += self.current_time
+		# self.buzzer_start_time += self.current_time
+		# self.system_start_time += self.current_time
 		
-		self.buzzer = Buzzer.Buzzer(self.buzzer_pin, self.current_time, self.buzzer_start_time, self.altitude_threshold) #Create Buzzer Object
-		self.CutDown = CutDown.CutDown(self.current_time, self.cut_down_pin, self.cut_time) #Create CutDown Object
+		# self.buzzer = Buzzer.Buzzer(self.buzzer_pin, self.current_time, self.buzzer_start_time, self.altitude_threshold) #Create Buzzer Object
+		# self.CutDown = CutDown.CutDown(self.current_time, self.cut_down_pin, self.cut_time) #Create CutDown Object
 		
 			
 	def loop(self):
-		self.current_time = time.time()*1000.0 #Get current time
+	
+		if(self.timer_buzzer_poll.get_flag == 0):
+			self.buzzer.loop(5) #Run buzzer subroutine	
+			self.timer_buzzer_poll.start_timer()
+	
+		# self.current_time = time.time()*1000.0 #Get current time
 	
 		#start up 
-		if(self.current_time < self.system_start_time):
-			self.buzzer.beep_delay(self.current_time)
+		# if(self.current_time < self.system_start_time):
+			# self.buzzer.beep_delay(self.current_time)
 	
 		#Main loop for reading data, cut rope and buzzer
-		else:
-			if(self.current_time - self.previous_data_read_time > self.data_read_time):
+		# else:
+			# if(self.current_time - self.previous_data_read_time > self.data_read_time):
 				#Run data read subroutine
-				self.previous_data_read_time = self.current_time #reset timer
+				# self.previous_data_read_time = self.current_time #reset timer
 				
-			if(self.current_time - self.previous_buzzer_time > self.buzzer_time):
-				self.buzzer.loop(self.current_time, 5) #Run buzzer subroutine
-				self.previous_buzzer_time = self.current_time #reset timer
+			# if(self.current_time - self.previous_buzzer_time > self.buzzer_time):
+				# self.buzzer.loop(self.current_time, 5) #Run buzzer subroutine
+				# self.previous_buzzer_time = self.current_time #reset timer
 				
-			if(self.current_time - self.previous_cut_time > self.cut_timer):
-				self.CutDown.cut(self.current_time) #Run cut down subroutine
-				self.previous_cut_time = self.current_time #reset timer
+			# if(self.current_time - self.previous_cut_time > self.cut_timer):
+				# self.CutDown.cut(self.current_time) #Run cut down subroutine
+				# self.previous_cut_time = self.current_time #reset timer
 
 if __name__ == '__main__':
 	mchab = McHab2012()
